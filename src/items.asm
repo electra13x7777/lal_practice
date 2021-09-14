1c88770c747addc13aa27468989e9c58c9dd1710
Alex Barney <alex.f.barney@gmail.com>
2021-09-14 12:15:13 -0400

    Started working on item setting and warping for practice states

---
+++ src/items.asm
@@ -0,0 +1,28 @@
+; File: items.asm
+; Author: Alex Barney
+;
+; item offsets: 1714 - 1813
+; item ammounts: 1814 - 1913
+org $FFFFFF 
+
+; Bakumatsu Chapter
+
+; NINJA START
+
+; JAIL
+
+; B3 A1
+
+; B3 A2
+
+; KARAKURI
+
+; MONKS
+
+; MUSASHI
+
+; YODO KUN
+
+; ODE IOU
+
+; GAMAHEBI
---
+++ src/poswarp.asm
@@ -0,0 +1,27 @@
+; Bakumatsu Chapter
+
+; NINJA START (40, 35)
+    lda #$28    ; Load A with X cord
+    sta $1654   ; Store contents of A in addr
+    lda #$23    ; Load A with Y cord
+    sta $163C   ; Store contents of A in addr
+; JAIL (32, 39)
+    lda #$20    ; Load A with X cord
+    sta $1654   ; Store contents of A in addr
+    lda #$27    ; Load A with Y cord
+    sta $163C   ; Store contents of A in addr
+; B3 A1
+
+; B3 A2
+
+; KARAKURI
+
+; MONKS
+
+; MUSASHI
+
+; YODO KUN
+
+; ODE IOU
+
+; GAMAHEBI
